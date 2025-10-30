dca1fe79da3eb39efe292a593eb9ecbb3d4a552b
TARAKESHWARAN V <E0324003@sriher.edu.in>
2025-10-30 13:14:56 +0530

    Create scatterplot.py

---
+++ scatterplot.py
@@ -0,0 +1,7 @@
+customers = [200,240,300,400,420,390,440,480,460,500,470,550]
+
+plt.scatter(customers, sales, color='red')
+plt.title("Sales vs Number of Customers")
+plt.xlabel("Customers")
+plt.ylabel("Sales (₹)")
+plt.show()
